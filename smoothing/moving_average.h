1609c84b3ebf2907812313f69899dfa82c5ecb70
Izadori <blueocean.okinawa@gmail.com>
2026-03-12 16:13:44 +0900

    modify comment in smoothing/moving_average.h

--- smoothing/moving_average.h
+++ smoothing/moving_average.h
@@ -21,7 +21,7 @@ namespace sablib {
  * @brief Calculates the simple moving average of the input signal (std::vector<double> version).
  *
  * @param y The data to be averaged.
- * @param n Number of points for calculating the average (calculated using `2 * n + 1` points).
+ * @param n Half-width of the moving average window (calculated using `2 * n + 1` points).
  * @return The data after applying the moving average.
  */
 const std::vector<double> MovingAverage(const std::vector<double> & y, const unsigned int n);
@@ -30,7 +30,7 @@ const std::vector<double> MovingAverage(const std::vector<double> & y, const uns
  * @brief Calculates the simple moving average of the input signal.
  *
  * @param y The data to be averaged.
- * @param n Number of points for calculating the average (calculated using `2 * n + 1` points).
+ * @param n Half-width of the moving average window (calculated using `2 * n + 1` points).
  * @return The data after applying the moving average.
  */
 template <typename Derived>
