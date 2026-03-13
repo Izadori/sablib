06d7760f979c5cee657398768b8149c6ac0b4871
Izadori <blueocean.okinawa@gmail.com>
2026-03-13 15:06:41 +0900

    modify comments in airpls.h

--- baseline/airpls.h
+++ baseline/airpls.h
@@ -5,6 +5,7 @@
  * @details
  * References:
  * @li Zhang, Z.-M.; Chena, Shan; Liang, Y.-Z. Baseline correction using adaptive iteratively reweighted penalized least squares Analyst 2010, 135(5), 1138-1146.
+ * @li <a href="https://github.com/zmzhang/airPLS">Zhang's GitHub</a>
  */
 
 #ifndef __SABLIB_AIRPLS_H__
