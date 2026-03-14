08ece8ee3aaa91191fd62800be5cc1f38162be33
Izadori <blueocean.okinawa@gmail.com>
2026-03-15 01:09:54 +0900

    modify misc/polyfit.h

--- misc/polyfit.h
+++ misc/polyfit.h
@@ -28,29 +28,30 @@ template <typename Derived>
 const typename Derived::PlainObject
 PolyFit(const Eigen::MatrixBase<Derived> & x, const Eigen::MatrixBase<Derived> & y, const unsigned int polyorder)
 {
-    using Scalar = typename Derived::PlainObject::Scalar;
-    const int n = x.size();
-    const int m = polyorder + 1;
+	using Scalar = typename Derived::PlainObject::Scalar;
+	const int n = x.size();
+	const int m = polyorder + 1;
 
-    if (x.size() != y.size()) {
-        throw std::invalid_argument("PolyFit(): x and y must have the same size.");
-    }
+	if(x.size() != y.size()) {
+		throw std::invalid_argument("PolyFit(): x and y must have the same size.");
+	}
 
-    if (m > n) {
-        throw std::invalid_argument("PolyFit(): polyorder is too high for the number of points.");
-    }
+	if(m > n) {
+		throw std::invalid_argument("PolyFit(): polyorder is too high for the number of points.");
+	}
 
-    Eigen::MatrixX<Scalar> A(n, m);
+	Eigen::MatrixX<Scalar> A(n, m);
 
-	for (int i = 0; i < n; ++i) {
-        Scalar val = 1.0;
-        for (int j = 0; j < m; ++j) {
-            A(i, j) = val;
-            val *= x(i);
-        }
-    }
+	for(int i = 0; i < n; ++i) {
+		Scalar val = 1.0;
 
-    return (A.transpose() * A).ldlt().solve(A.transpose() * y);
+		for(int j = 0; j < m; ++j) {
+			A(i, j) = val;
+			val *= x(i);
+		}
+	}
+
+	return (A.transpose() * A).ldlt().solve(A.transpose() * y);
 }
 
 }; // namespace sablib
