1054234ef85e78a669dbca849723ccb99639bb90
Izadori <blueocean.okinawa@gmail.com>
2026-03-18 01:09:01 +0900

    add sablib.h

---
+++ sablib.h
@@ -0,0 +1,22 @@
+/**
+ * @file sablib.h
+ * @brief Header file of sablib, a C++ library for signal smoothing and baseline estimation
+ * @author Izadori
+ */
+
+#ifndef __SABLIB_H__
+#define __SABLIB_H__
+
+#include "smoothing/moving_average.h"
+#include "smoothing/savitzky_golay.h"
+#include "smoothing/whittaker.h"
+#include "smoothing/pspline.h"
+
+#include "baseline/sma.h"
+#include "baseline/polynomial.h"
+#include "baseline/spline.h"
+#include "baseline/asls.h"
+#include "baseline/airpls.h"
+#include "baseline/arpls.h"
+
+#endif // __SABLIB_H__
