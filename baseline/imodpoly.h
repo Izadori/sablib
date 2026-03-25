f33421e8d02fb7cc1af246cbdff77a8e34318562
Izadori <blueocean.okinawa@gmail.com>
2026-03-26 02:38:12 +0900

    modify baseline/imodpoly.h

--- baseline/imodpoly.h
+++ baseline/imodpoly.h
@@ -4,7 +4,7 @@
  * @author Izadori
  * @details
  * References:
- * @li Zhao, J.; Lui, H.; McLean, D. I.; Zeng, H. Automated Autofluorescence Background Subtraction Algorithm for Biomedical Raman Spectroscopy" Applied Spectroscopy 2007, 61(11), 1225-1232.
+ * @li Zhao, J.; Lui, H.; McLean, D. I.; Zeng, H. "Automated Autofluorescence Background Subtraction Algorithm for Biomedical Raman Spectroscopy" Applied Spectroscopy 2007, 61(11), 1225-1232.
  */
 
 #ifndef __SABLIB_IMODPOLY_H__
@@ -24,7 +24,7 @@ namespace sablib {
  * @param loop The maximum number of iterations (default is 50).
  * @param eps The convergence tolerance based on the standard deviation change (default is 1.0e-3).
  * @return A vector containing the estimated baseline.
- * @throw std::invalid_argument One or more parameters are wrong.
+ * @exception std::invalid_argument One or more parameters are wrong.
  */
 const std::vector<double> BaselineIModPoly(
 	const std::vector<double> & y, const unsigned int polyorder, const double k = 1,
